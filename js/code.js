f0b7222ae585b29ad0b1811040e8ea57ed1d503d
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-28 23:12:38 +0200

    add about content - not done

--- js/code.js
+++ js/code.js
@@ -1,18 +1,1 @@
 
-
-
-
-$(document).ready(function () {
-
-    $('#content').load('content/index.html');
-
-$('ul#nav li a').click(function () {
-
-   var page = $(this).attr('href');
-
-   $('#content').load('content/' + page + '.html');
-
-   return false;
-});
-
-});
